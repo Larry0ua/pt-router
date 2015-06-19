ae972fc4aa37658bf7b7c19c065792e58341c901
Valerii Pekarskyi <pekarskiy.valeriy@gmail.com>
2015-06-19 12:13:31 +0300

    Indentation fix

--- src/main/groovy/dospring/service/RouteOrderingService.groovy
+++ src/main/groovy/dospring/service/RouteOrderingService.groovy
@@ -22,8 +22,8 @@ class RouteOrderingService {
         // time for route: 10 * number of switches from route to route + 2 * each stop travelled (average) + 6 * distance walked (km)
         Map<CalculatedRoute, BigDecimal> sortedRoutes = routes.collectEntries {[it,
                                 switchTime * it.routeChunks.count{it.routes} + // each route switch would take 10 minutes
-                                        walkSpeed * (it.routeChunks.findAll{!it.routes}.sum {it.start - it.end}?:0) + // 6 minutes for each walked km
-                                        stopTime * (it.routeChunks.findAll{it.routes}.sum{ RouteChunk rc ->
+                                walkSpeed * (it.routeChunks.findAll{!it.routes}.sum {it.start - it.end}?:0) + // 6 minutes for each walked km
+                                stopTime * (it.routeChunks.findAll{it.routes}.sum{ RouteChunk rc ->
                                     rc.routes.sum{Route r-> r.countBetween(rc.start, rc.end)} / rc.routes.size()
                                 }?:0) // 2 * average count of stops travelled
         ]}
